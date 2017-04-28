7ac384d11773f91f694fa0d1da8738fb2499c742
Salva <salvadorcerdaholmager@enti.cat>
2017-04-28 13:19:01 +0200

    FIN

--- P2_G47/Alchemy/Alchemy.cpp
+++ P2_G47/Alchemy/Alchemy.cpp
@@ -3,21 +3,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<stdio.h>
+#include <stdio.h>
 #include <windows.h>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
 
 
-unsigned int split(const std::string &txt, std::vector<std::string> &strs, char ch)
+unsigned int split(const std::string &txt, std::vector<std::string> &strs, std::string ch)
 {
 	unsigned int pos = txt.find(ch);
-	unsigned int initialPos = 0;
+    int initialPos = 0 ;
 	strs.clear();
-
+	int offset = 0;
 	// Decompose statement
 	while (pos != std::string::npos) {
 		strs.push_back(txt.substr(initialPos, pos - initialPos));
-		initialPos = pos + 1;
-
+		initialPos = pos + ch.length();
 		pos = txt.find(ch, initialPos);
 	}
 
@@ -31,6 +33,8 @@ unsigned int split(const std::string &txt, std::vector<std::string> &strs, char 
 
 //Creamos una lista global
 std::vector<std::string> *lista = new std::vector<std::string>;
+
+std::unordered_map<std::string, std::string> map;
 
 //Función que imprime las instrucciones del programa
 void showHelp() {
@@ -56,6 +60,30 @@ void showList() {
 
 
 void main() {
+	std::ifstream infile("elements.dat");
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		std::vector<std::string> parts;
+		split(line, parts, " = ");
+std::string resultat = parts.at(0);
+std::string resultat1 = parts.at(1);
+
+
+		std::vector<std::string> parts2;
+		split(parts.at(1), parts2, " + ");
+
+		
+		map[parts2.at(0) + " + " + parts2.at(1)] = parts.at(0);
+	}
+	std::vector<std::string> ans;
+	split("combine 3 1", ans, " ");
+	std::cout << ans.at(0) << "|" << ans.at(1) << "|" << ans.at(2) << std::endl;
+
+	std::vector<std::string> ans2;
+	split("Lawn Mower = Scissors + Combustion Engine", ans2, " = ");
+	std::cout << ans2.at(0) << "|" << ans2.at(1) << std::endl;
+
 	//Datos introducidos por el usuario
 	std::string userCommand;
 
@@ -71,7 +99,7 @@ void main() {
 	while (true) {
 		std::getline(std::cin, userCommand);
 		std::vector<std::string>  arguments;
-		split(userCommand, arguments, ' ');
+		split(userCommand, arguments, " ");
 
 
 		//Si el usuario introduce HELP
@@ -96,16 +124,57 @@ void main() {
 			ShellExecute(NULL, "open", link.c_str() , NULL, NULL, SW_SHOWNORMAL);
 		}
 
+
 	
 		//Si el usuario introduce ADD X
-		if (arguments.at(0).compare("add") == 0) {
+		if (arguments.at(0).compare("add") == 0 && !arguments.at(1).compare("basics") == 0) {
 			int posicion = stoi(arguments.at(1));
 			std::cout <<"\n" << std::endl;
 			lista->push_back((lista->at(posicion)));
-
-
 		}
-		
+
+		//Si el usuario introduce ADD BASICS
+		if (arguments.at(0).compare("add") == 0 && arguments.at(1).compare("basics") == 0) {
+
+			lista->push_back("Air");
+			lista->push_back("Earth");
+			lista->push_back("Fire");
+			lista->push_back("Water");
+		}
+		if (arguments.at(0) =="clean") {
+			std::vector<std::string> *newLista = new std::vector<std::string>;
+			for (int i = 0; i < lista->size();i++) {
+				std::string actual = lista->at(i);
+				if (std::find(newLista->begin(), newLista->end(), actual) != newLista->end()) {
+
+				}
+				else {
+					newLista->push_back(actual);
+				}
+			}
+			delete lista;
+			lista = newLista;
+		}
+
+		if (arguments.at(0).compare("sort") == 0) {
+			std::sort(lista->begin(), lista->end());
+		}
+
+		if (arguments.at(0)=="combine" ) {
+			std::string elemento1 = lista->at(stoi(arguments.at(1)));
+			std::string elemento2 = lista->at(stoi(arguments.at(2)));
+			
+			std::cout << "Combinando " << elemento1 << " con " << elemento2 << std::endl;
+
+			std::string key = elemento1 + " + " + elemento2;
+			
+			lista->push_back(map[key]);
+			lista->erase(lista->begin() + std::stoi(arguments.at(1)));
+			lista->erase(lista->begin() + std::stoi(arguments.at(2)));
+		}
+
+
+
 		showList();
 	}
 
